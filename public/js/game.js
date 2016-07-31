042d2e83d01389f738f43466fb0ab71c783028d5
Zachery Miranda <zmiranda@mit.edu>
2016-07-30 18:49:52 -0700

    communicate board object between client/server

--- public/js/game.js
+++ public/js/game.js
@@ -1,74 +1,89 @@
 var pics = {"x": "img/x.png", "o": "img/o.png", "": ""}
 
 class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      board: [['','',''], ['','',''], ['','','']],
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
-  handleClick(x,y) {
-  	var board = this.state.board
-  	board[x][y] = 'x'
-  	const x2 = Math.floor((Math.random() * 3) + 0);
-  	const y2 = Math.floor((Math.random() * 3) + 0);
-  	board[x2][y2] = 'o'
-    this.setState({board: board});
-  }
-  render() {
-  return (
-    	<table>
-	      	<tbody>
-	      	<tr>
-	      		<Square board={this.state.board} handleClick={this.handleClick} x="0" y="0"/>
-	      		<Square board={this.state.board} handleClick={this.handleClick} x="0" y="1"/>
-	      		<Square board={this.state.board} handleClick={this.handleClick} x="0" y="2"/>
-	      	</tr>
-	      	<tr>
-	      		<Square board={this.state.board} handleClick={this.handleClick} x="1" y="0"/>
-	      		<Square board={this.state.board} handleClick={this.handleClick} x="1" y="1"/>
-	      		<Square board={this.state.board} handleClick={this.handleClick} x="1" y="2"/>
-	      	</tr>
-	      	<tr>
-	      		<Square board={this.state.board} handleClick={this.handleClick} x="2" y="0"/>
-	      		<Square board={this.state.board} handleClick={this.handleClick} x="2" y="1"/>
-	      		<Square board={this.state.board} handleClick={this.handleClick} x="2" y="2"/>
-	      	</tr>
-	      	</tbody>
-	      </table>
-    	);	
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            board: [['','',''], ['','',''], ['','','']],
+        };
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick(x,y) {
+        // update based on click
+        var board = this.state.board
+        board[x][y] = 'x'
+
+        // get AI move
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("POST", "/move", true);
+        xhttp.setRequestHeader("Content-Type", "application/json");
+        
+        var data = JSON.stringify({"board": board})
+        xhttp.send(data);
+
+        xhttp.onreadystatechange = function() {
+            if (xhttp.readyState == 4 && xhttp.status == 200) {
+                console.log(xhttp.responseText);
+            }
+        };
+
+        const x2 = Math.floor((Math.random() * 3) + 0);
+        const y2 = Math.floor((Math.random() * 3) + 0);
+        board[x2][y2] = 'o'
+        this.setState({board: board});
+    }
+    render() {
+        return (
+            <table>
+            <tbody>
+            <tr>
+            <Square board={this.state.board} handleClick={this.handleClick} x="0" y="0"/>
+            <Square board={this.state.board} handleClick={this.handleClick} x="0" y="1"/>
+            <Square board={this.state.board} handleClick={this.handleClick} x="0" y="2"/>
+            </tr>
+            <tr>
+            <Square board={this.state.board} handleClick={this.handleClick} x="1" y="0"/>
+            <Square board={this.state.board} handleClick={this.handleClick} x="1" y="1"/>
+            <Square board={this.state.board} handleClick={this.handleClick} x="1" y="2"/>
+            </tr>
+            <tr>
+            <Square board={this.state.board} handleClick={this.handleClick} x="2" y="0"/>
+            <Square board={this.state.board} handleClick={this.handleClick} x="2" y="1"/>
+            <Square board={this.state.board} handleClick={this.handleClick} x="2" y="2"/>
+            </tr>
+            </tbody>
+            </table>
+            );  
+    }
 }
 
 class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: pics[props.board[props.x][props.y]],
-      x: props.x,
-      y: props.y
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
-  handleClick() {
-    // this.setState({value: pics['x']});
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: pics[props.board[props.x][props.y]],
+            x: props.x,
+            y: props.y
+        };
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick() {
     this.props.handleClick(this.state.x, this.state.y)
-  }
-  componentWillReceiveProps(props) {
-  	let pic = pics[props.board[this.state.x][this.state.y]]
-  	this.setState({value: pic});
-  }
-  render() {
-    return (
-      <td onClick={this.handleClick}>
-        <img src={this.state.value}/>
-      </td>
-    );
-  }
+    }
+    componentWillReceiveProps(props) {
+        let pic = pics[props.board[this.state.x][this.state.y]]
+        this.setState({value: pic});
+    }
+    render() {
+        return (
+            <td onClick={this.handleClick}>
+            <img src={this.state.value}/>
+            </td>
+            );
+    }
 }
 
 ReactDOM.render(
-  <Board />,
-  document.getElementById('content')
-);
+    <Board />,
+    document.getElementById('content')
+    );
--- server.go
+++ server.go
@@ -1,15 +1,36 @@
 package main
 
 import (
-	_ "fmt"
+	"fmt"
 	"log"
 	"net/http"
+	"encoding/json"
 )
 
+type Board struct {
+	Board [][]string
+}
+
+func moveHandler(w http.ResponseWriter, r *http.Request) {
+    b := Board{}
+
+    if r.Body == nil {
+        http.Error(w, "Please send a request body", 400)
+        return
+    }
+    err := json.NewDecoder(r.Body).Decode(&b)
+    if err != nil {
+        http.Error(w, err.Error(), 400)
+        return
+    }
+    fmt.Println(b.Board)
+    json.NewEncoder(w).Encode(b)
+    // fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
 
 func main() {
 	port := "3000"
-	
+	http.HandleFunc("/move", moveHandler)
 	http.Handle("/", http.FileServer(http.Dir("./public")))
 	log.Println("Server started: http://localhost:" + port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
